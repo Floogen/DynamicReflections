fb588693207151f7fdfc861e93e531d514d616d6
Floogen <31755155+Floogen@users.noreply.github.com>
2022-07-11 19:12:46 -0500

    Added splash effect on puddles while raining

--- DynamicReflections/Framework/Patches/Locations/GameLocationPatch.cs
+++ DynamicReflections/Framework/Patches/Locations/GameLocationPatch.cs
@@ -18,6 +18,7 @@ namespace DynamicReflections.Framework.Patches.Tools
     internal class GameLocationPatch : PatchTemplate
     {
         private readonly Type _type = typeof(GameLocation);
+        private static double _elapsedMilliseconds;
 
         internal GameLocationPatch(IMonitor modMonitor, IModHelper modHelper) : base(modMonitor, modHelper)
         {
@@ -42,7 +43,6 @@ namespace DynamicReflections.Framework.Patches.Tools
             var playerTilePosition = Game1.player.getTileLocationPoint();
             if (__instance.lastTouchActionLocation.Equals(Vector2.Zero) && Int32.TryParse(__instance.doesTileHaveProperty(playerTilePosition.X, playerTilePosition.Y, "PuddleIndex", "Back"), out int puddleIndex) && puddleIndex != PuddleManager.DEFAULT_PUDDLE_INDEX)
             {
-                // TODO: Play splash sound
                 float xOffset = Game1.player.FacingDirection == 3 ? 64f : 0f;
                 float yOffset = Game1.player.FacingDirection == 0 ? 64f : 0f;
                 switch (Game1.player.FacingDirection)
@@ -67,6 +67,29 @@ namespace DynamicReflections.Framework.Patches.Tools
 
 
                 __instance.lastTouchActionLocation = new Vector2(playerTilePosition.X, playerTilePosition.Y);
+            }
+
+            _elapsedMilliseconds += time.ElapsedGameTime.TotalMilliseconds;
+            if (_elapsedMilliseconds > 500)
+            {
+                _elapsedMilliseconds = 0;
+
+                var puddles = DynamicReflections.puddleManager.GetPuddleTiles(__instance, limitToView: true);
+                if (puddles.Count > 0)
+                {
+                    for (int i = 0; i < Game1.random.Next(1, 5); i++)
+                    {
+                        var puddleTile = puddles[Game1.random.Next(puddles.Count)];
+                        if (Game1.random.NextDouble() > 0.5)
+                        {
+                            TemporaryAnimatedSprite splashSprite = new TemporaryAnimatedSprite("TileSheets\\animations", new Microsoft.Xna.Framework.Rectangle(0, 0, 64, 64), Game1.random.Next(50, 100), 9, 1, new Vector2(puddleTile.X, puddleTile.Y) * 64f, flicker: false, flipped: false, 0f, 0f, new Color(255, 255, 255, 155), 1f, 0f, 0f, 0f);
+                            DynamicReflections.puddleManager.puddleRippleSprites.Add(splashSprite);
+
+                            TemporaryAnimatedSprite dropletSprite = new TemporaryAnimatedSprite("TileSheets\\animations", new Microsoft.Xna.Framework.Rectangle(2 * 64, 18 * 64, 64, 64), Game1.random.Next(75, 125), 5, 1, new Vector2(puddleTile.X, puddleTile.Y - 0.5f) * 64f, flicker: false, flipped: false, 0f, 0f, new Color(141, 181, 216, 91), 1f, 0f, 0f, 0f);
+                            __instance.temporarySprites.Add(dropletSprite);
+                        }
+                    }
+                }
             }
         }
     }
