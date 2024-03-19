7fa5f34aa952540abd76d01deca2161a99a437f3
Floogen <31755155+Floogen@users.noreply.github.com>
2024-03-19 15:04:56 -0500

    Handled Game1.getTrulyDarkTime change

--- DynamicReflections/DynamicReflections.cs
+++ DynamicReflections/DynamicReflections.cs
@@ -259,7 +259,7 @@ namespace DynamicReflections
             }
 
             // Handle the sky reflections
-            var targetDarkTime = Game1.getTrulyDarkTime() + 100;
+            var targetDarkTime = Game1.getTrulyDarkTime(Game1.currentLocation) + 100;
             DynamicReflections.shouldDrawNightSky = false;
             if (modConfig.AreSkyReflectionsEnabled is not false && currentSkySettings is not null && currentSkySettings.AreReflectionsEnabled && Game1.currentLocation.IsOutdoors && Game1.IsRainingHere(Game1.currentLocation) is false && Game1.timeOfDay >= targetDarkTime)
             {
